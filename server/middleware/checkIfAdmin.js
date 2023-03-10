621114ae0cf3cba60c4adee62af76c6abd9d8d80
Mondi321 <eh52213@ubt-uni.net>
2023-03-10 21:41:05 +0100

    admin changes

--- server/middleware/checkIfAdmin.js
+++ server/middleware/checkIfAdmin.js
@@ -4,15 +4,16 @@ const checkIfAdmin = async (req, res, next) => {
   const {authToken} = req;
   try {
     const user = await admin.auth().verifyIdToken(authToken);
+    if (user.admin) {
+      req.authId = user.uid;
+    } else {
+      return res.status(403).json({message: "Not an admin"});
+    }
   } catch (err) {
     return res.status(401).json({message: "Cannot verify right now"});
   }
 
-  if (user.admin) {
-    req.authId = user.uid;
-  } else {
-    return res.status(403).json({message: "Not an admin"});
-  }
+
   next();
 };
 
